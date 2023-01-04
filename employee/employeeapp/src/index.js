ff44e50eec214bbde11382cce6ce74baa6f504cc
zcomert <zafer.comert@hotmail.com>
2023-01-04 14:20:08 +0300

    createTheme has been applied.

--- employee/employeeapp/src/index.js
+++ employee/employeeapp/src/index.js
@@ -2,16 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import Counter from "./components/counter/Counter";
 import { AppProvider } from "./context/AppContext";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { purple, red, yellow } from "@mui/material/colors";
+
+const theme1 = createTheme({
+  palette: {
+    primary: purple,
+    secondary: yellow,
+  },
+});
+
+const theme2 = createTheme({
+  palette: {
+    primary: red,
+    secondary: yellow,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AppProvider>
+    <ThemeProvider theme={theme2}>
+      <AppProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AppProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
