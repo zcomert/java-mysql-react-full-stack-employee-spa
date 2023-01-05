3511595e57a8153bc7035b1db1386a5646f2227b
zcomert <zafer.comert@hotmail.com>
2023-01-05 21:39:08 +0300

    Extension method for methodargumentnotvalid has been defined.

--- employeebackend/src/main/java/com/employee/employee/extensions/RestResponseEntityExceptionHandler.java
+++ employeebackend/src/main/java/com/employee/employee/extensions/RestResponseEntityExceptionHandler.java
@@ -1,8 +1,12 @@
 package com.employee.employee.extensions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -27,4 +31,17 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
                         .build());
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, String> validationErrors = new HashMap<String, String>();
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(validationErrors);
+    }
 }
